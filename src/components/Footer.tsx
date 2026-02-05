6ce419d638758e1c81e2634080a87717db23eafd
huzaifa baloch <huzaifa.baloch.9t@gmail.com>
2026-02-04 22:11:17 -0800

    update footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,7 +2,7 @@ export default function Footer() {
   return (
     <footer className="text-center py-6 bg-gray-900 text-white">
       <h1>footer</h1>
-      
+      <p>Email:Huzaifa@gmail.com</p>
     </footer>
   );
 }
