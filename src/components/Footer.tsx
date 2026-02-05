08c5df22eeae56c4e7e828142d505029f5cbfd48
huzaifa baloch <huzaifa.baloch.9t@gmail.com>
2026-02-04 22:10:04 -0800

    update footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,8 @@
 export default function Footer() {
   return (
     <footer className="text-center py-6 bg-gray-900 text-white">
-      © 2026 MyStore
+      <h1>footer</h1>
+      
     </footer>
   );
 }
